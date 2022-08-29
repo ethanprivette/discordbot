704df416b412ff4c02397f787efa6a9668b59a98
ethanprivette <112294818+ethanprivette@users.noreply.github.com>
2022-08-29 00:50:31 -0400

    updated train command

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on('interactionCreate', async interaction => {
 	const { commandName } = interaction;
 
 	if (commandName === 'train') {
-		const row = new ActionRowBuilder()
+		const row1 = new ActionRowBuilder()
             .addComponents(
                 new ButtonBuilder()
                     .setCustomId('Infantry')
@@ -40,7 +40,7 @@ client.on('interactionCreate', async interaction => {
                     .setLabel('Navy')
                     .setStyle(ButtonStyle.Primary),
             );
-        await interaction.reply({ content: 'Which unit would you like to train?', components: [row]});
+        await interaction.reply({ content: 'Which unit would you like to train?', ephemeral: true, components: [row1]});
             }
 });
 
@@ -48,52 +48,57 @@ client.on('interactionCreate', async interaction =>{
     if (!interaction.isButton()) return;
 
     if (interaction.customId == 'Infantry'){
-        const row = new ActionRowBuilder()
+        const row2 = new ActionRowBuilder()
         .addComponents(
             new ButtonBuilder()
                 .setCustomId('1k Infantry')
                 .setLabel('1k Infantry')
-                .setStyle(ButtonStyle.Primary),
+                .setStyle(ButtonStyle.Primary)
         );
-            await interaction.reply({ content: 'How many Infantry would you like to train?', components: [row]})
-
+            await interaction.reply({ content: 'How many Infantry would you like to train?', ephemeral: true, components: [row2]})
     }else if (interaction.customId == 'Tanks'){
-        const row = new ActionRowBuilder()
+        const row3 = new ActionRowBuilder()
         .addComponents(
             new ButtonBuilder()
                 .setCustomId('8 Tanks')
                 .setLabel('8 Tanks')
                 .setStyle(ButtonStyle.Primary),
         );
-            await interaction.reply({ content: 'How many Tanks would you like to train?', components: [row]})
+            await interaction.reply({ content: 'How many Tanks would you like to train?', ephemeral: true, components: [row3]})
 
     }else if (interaction.customId == 'Planes'){
-        const row = new ActionRowBuilder()
+        const row4 = new ActionRowBuilder()
         .addComponents(
             new ButtonBuilder()
                 .setCustomId('4 Planes')
                 .setLabel('4 Planes')
                 .setStyle(ButtonStyle.Primary),
         );
-            await interaction.reply({ content: 'How many Planes would you like to train?', components: [row]})
+            await interaction.reply({ content: 'How many Planes would you like to train?', ephemeral: true, components: [row4]})
     
     }else if (interaction.customId == 'Navy'){
-        const row = new ActionRowBuilder()
+        const row5 = new ActionRowBuilder()
         .addComponents(
             new ButtonBuilder()
                 .setCustomId('1 Ship')
                 .setLabel('1 Ship')
                 .setStyle(ButtonStyle.Primary),
         );
-            await interaction.reply({ content: 'How many ships would you like to train?', components: [row]})
+            await interaction.reply({ content: 'How many ships would you like to train?', ephemeral: true, components: [row5]})
     }
 });
 
 client.on('interactionCreate', async interaction =>{
     if (!interaction.isButton()) return;
 
-    if (interaction.customId == '4 planes'){
+    if (interaction.customId == '1k Infantry'){
+        await interaction.reply('Infantry will be ready in 30 minutes.')
+    }else if (interaction.customId == '8 Tanks'){
+        await interaction.reply('Tanks will be ready in 35 minutes.')
+    }else if (interaction.customId == '4 Planes'){
         await interaction.reply('Planes will be ready in 1 hour.')
+    }else if (interaction.customId == '1 Ship'){
+        await interaction.reply('Ship will be ready in 3 hours.')
     }
 });
 
