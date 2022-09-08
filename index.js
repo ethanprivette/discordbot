a5fef8b406ba4b448b046d66df6a345c2567c59f
Wooper <minecrafter21645@gmail.com>
2022-09-08 18:08:15 -0400

    test

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ client.on('interactionCreate', async interaction => {
     }
 });
 /*
-Tes
+testing
 */
 client.on('interactionCreate', async interaction =>{
 	if (!interaction.isChatInputCommand()) return;
