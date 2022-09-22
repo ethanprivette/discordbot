0b0ae06bc11d8ecc143560dfc14e5f22d82abede
ethanprivette <112294818+ethanprivette@users.noreply.github.com>
2022-09-22 14:38:46 -0400

    literally balls

--- index.js
+++ index.js
@@ -66,11 +66,9 @@ function log(msg, key) {
 				    channel.then(channel=>channel.send(`**${now.toLocaleString()}** : *${msg}*`))
 	    });
         } else {
-            client.on('ready', client => {
 		        console.log(key + ' = key ');
 		        const channel = client.channels.fetch('1017927935488966697');
 			        channel.then(channel=>channel.send(`**${now.toLocaleString()}** : *${msg}*`))
-            })
         }
     }
 
