a49e7a50e4fa63e4f4bb9f29363514d3ae5aa7a2
ethanprivette <112294818+ethanprivette@users.noreply.github.com>
2022-09-22 13:10:01 -0400

    moved currency system

--- index.js
+++ index.js
@@ -15,121 +15,7 @@ const client = new Client({
     },
     intents: [botIntents],
 });
-
-function log(msg, key) {
-	console.log(msg);
-	var now = new Date();
-    client.on('ready', client => {
-	    if (key === undefined) {
-			const channel = client.channels.fetch('1017927935488966697');
-				channel.then(channel=>channel.send(`${now.toUTCString()}: ${msg}`))
-
-        } else {
-		    var client = key;
-		    var channel = client.channels.fetch('1017927935488966697');
-			    channel.then(channel=>channel.send(`${now.toUTCString()}: ${msg}`))
-        }
-    })
-}
-
-
 const currency = new Collection();
-
-Reflect.defineProperty(currency, 'add', {
-	value: async (id, amount) => {
-		const user = currency.get(id);
-
-		if (user) {
-			user.balance += Number(amount);
-			return user.save();
-		}
-
-		const newUser = await Users.create({ user_id: id, balance: amount });
-		currency.set(id, newUser);
-
-		return newUser;
-	},
-});
-
-Reflect.defineProperty(currency, 'getBalance', {
-	value: id => {
-		const user = currency.get(id);
-		return user ? user.balance : 0;
-	},
-});
-
-client.once('ready', async () => {
-    const storedBalances = await Users.findAll();
-	storedBalances.forEach(b => currency.set(b.user_id, b));
-
-	console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('messageCreate', async message => {
-	if (message.author.bot) return;
-	currency.add(message.author.id, 1);
-});
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const { commandName } = interaction;
-	if (commandName === 'balance') {
-        const target = interaction.options.getUser('user') ?? interaction.user;
-
-        return interaction.reply(`${target.tag} has ${currency.getBalance(target.id)}💰`);
-    } 
-    else if (commandName === 'inventory') {
-        const target = interaction.options.getUser('user') ?? interaction.user;
-        const user = await Users.findOne({ where: { user_id: target.id } });
-        const items = await user.getItems();
-    
-        if (!items.length) return interaction.reply(`${target.tag} has nothing!`);
-    
-        return interaction.reply(`${target.tag} currently has ${items.map(i => `${i.amount} ${i.item.name}`).join(', ')}`);
-    } 
-    else if (commandName === 'transfer') {
-        const currentAmount = currency.getBalance(interaction.user.id);
-        const transferAmount = interaction.options.getInteger('amount');
-        const transferTarget = interaction.options.getUser('user');
-    
-        if (transferAmount > currentAmount) return interaction.reply(`Sorry ${interaction.user}, you only have ${currentAmount}.`);
-        if (transferAmount <= 0) return interaction.reply(`Please enter an amount greater than zero, ${interaction.user}.`);
-    
-        currency.add(interaction.user.id, -transferAmount);
-        currency.add(transferTarget.id, transferAmount);
-    
-        return interaction.reply(`Successfully transferred ${transferAmount}💰 to ${transferTarget.tag}. Your current balance is ${currency.getBalance(interaction.user.id)}💰`);
-    } 
-    else if (commandName === 'buy') {
-        const itemName = interaction.options.getString('item');
-        const item = await CurrencyShop.findOne({ where: { name: { [Op.like]: itemName } } });
-    
-        if (!item) return interaction.reply(`That item doesn't exist.`);
-        if (item.cost > currency.getBalance(interaction.user.id)) {
-            return interaction.reply(`You currently have ${currency.getBalance(interaction.user.id)}, but the ${item.name} costs ${item.cost}!`);
-        }
-    
-        const user = await Users.findOne({ where: { user_id: interaction.user.id } });
-        currency.add(interaction.user.id, -item.cost);
-        await user.addItem(item);
-    
-        return interaction.reply(`You've bought: ${item.name}.`);
-    } 
-    else if (commandName === 'shop') {
-        const items = await CurrencyShop.findAll();
-        return interaction.reply(items.map(i => `${i.name}: ${i.cost}💰`).join('\n'));
-    } 
-    else if (commandName === 'leaderboard') {
-        return interaction.reply(
-                currency.sort((a, b) => b.balance - a.balance)
-                    .filter(user => client.users.cache.has(user.user_id))
-                    .first(10)
-                    .map((user, position) => `(${position + 1}) ${(client.users.cache.get(user.user_id).tag)}: ${user.balance}💰`)
-                    .join('\n'),
-        );
-    }
-});
 
 //log function test
 /*
@@ -170,6 +56,21 @@ con = {
 }
 */
 
+function log(msg, key) {
+	console.log(msg);
+	var now = new Date();
+    client.on('ready', client => {
+	    if (key === undefined) {
+			const channel = client.channels.fetch('1017927935488966697');
+				channel.then(channel=>channel.send(`${now.toUTCString()}: ${msg}`))
+
+        } else {
+		    var client = key;
+		    var channel = client.channels.fetch('1017927935488966697');
+			    channel.then(channel=>channel.send(`${now.toUTCString()}: ${msg}`))
+        }
+    })
+}
 
 //sequelize//
 
@@ -474,6 +375,102 @@ client.on('interactionCreate', async interaction => {
         if (!rowCount) return interaction.reply('That tag doesn\'t exist.');
     
         return interaction.reply('Tag deleted.');
+    }
+});
+
+Reflect.defineProperty(currency, 'add', {
+	value: async (id, amount) => {
+		const user = currency.get(id);
+
+		if (user) {
+			user.balance += Number(amount);
+			return user.save();
+		}
+
+		const newUser = await Users.create({ user_id: id, balance: amount });
+		currency.set(id, newUser);
+
+		return newUser;
+	},
+});
+
+Reflect.defineProperty(currency, 'getBalance', {
+	value: id => {
+		const user = currency.get(id);
+		return user ? user.balance : 0;
+	},
+});
+
+client.once('ready', async () => {
+    const storedBalances = await Users.findAll();
+	storedBalances.forEach(b => currency.set(b.user_id, b));
+
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', async message => {
+	if (message.author.bot) return;
+	currency.add(message.author.id, 1);
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+	if (commandName === 'balance') {
+        const target = interaction.options.getUser('user') ?? interaction.user;
+
+        return interaction.reply(`${target.tag} has ${currency.getBalance(target.id)}💰`);
+    } 
+    else if (commandName === 'inventory') {
+        const target = interaction.options.getUser('user') ?? interaction.user;
+        const user = await Users.findOne({ where: { user_id: target.id } });
+        const items = await user.getItems();
+    
+        if (!items.length) return interaction.reply(`${target.tag} has nothing!`);
+    
+        return interaction.reply(`${target.tag} currently has ${items.map(i => `${i.amount} ${i.item.name}`).join(', ')}`);
+    } 
+    else if (commandName === 'transfer') {
+        const currentAmount = currency.getBalance(interaction.user.id);
+        const transferAmount = interaction.options.getInteger('amount');
+        const transferTarget = interaction.options.getUser('user');
+    
+        if (transferAmount > currentAmount) return interaction.reply(`Sorry ${interaction.user}, you only have ${currentAmount}.`);
+        if (transferAmount <= 0) return interaction.reply(`Please enter an amount greater than zero, ${interaction.user}.`);
+    
+        currency.add(interaction.user.id, -transferAmount);
+        currency.add(transferTarget.id, transferAmount);
+    
+        return interaction.reply(`Successfully transferred ${transferAmount}💰 to ${transferTarget.tag}. Your current balance is ${currency.getBalance(interaction.user.id)}💰`);
+    } 
+    else if (commandName === 'buy') {
+        const itemName = interaction.options.getString('item');
+        const item = await CurrencyShop.findOne({ where: { name: { [Op.like]: itemName } } });
+    
+        if (!item) return interaction.reply(`That item doesn't exist.`);
+        if (item.cost > currency.getBalance(interaction.user.id)) {
+            return interaction.reply(`You currently have ${currency.getBalance(interaction.user.id)}, but the ${item.name} costs ${item.cost}!`);
+        }
+    
+        const user = await Users.findOne({ where: { user_id: interaction.user.id } });
+        currency.add(interaction.user.id, -item.cost);
+        await user.addItem(item);
+    
+        return interaction.reply(`You've bought: ${item.name}.`);
+    } 
+    else if (commandName === 'shop') {
+        const items = await CurrencyShop.findAll();
+        return interaction.reply(items.map(i => `${i.name}: ${i.cost}💰`).join('\n'));
+    } 
+    else if (commandName === 'leaderboard') {
+        return interaction.reply(
+                currency.sort((a, b) => b.balance - a.balance)
+                    .filter(user => client.users.cache.has(user.user_id))
+                    .first(10)
+                    .map((user, position) => `(${position + 1}) ${(client.users.cache.get(user.user_id).tag)}: ${user.balance}💰`)
+                    .join('\n'),
+        );
     }
 });
 
